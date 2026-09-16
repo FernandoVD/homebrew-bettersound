1a1bdd3c9e65e7890fa4cc351b5b9d644560e26b
FernandoVD <ftavandet@gmail.com>
2026-09-16 15:05:24 +0200

    Bump bettersound to v1.2.1

--- Casks/bettersound.rb
+++ Casks/bettersound.rb
@@ -1,6 +1,6 @@
 cask "bettersound" do
-  version "1.2.0"
-  sha256 "2bb3eea5483e60b009627d979447385b6739fe81216a663e4ceacc95e3205b12"
+  version "1.2.1"
+  sha256 "555b0464621399a7699a242c31477e632b35b81f34d10d7e122785ae921dfccd"
 
   url "https://github.com/FernandoVD/BetterSound/releases/download/v#{version}/BetterSound.zip"
   name "BetterSound"
